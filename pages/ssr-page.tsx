7e24f79eae93112e6cf6dd31621686b5b75e2e02
kyo144 <kwsmcy@gmail.com>
2022-08-22 01:10:56 +0800

    feature: make a ssr page test if things going well

--- pages/ssr-page.tsx
+++ pages/ssr-page.tsx
@@ -1,21 +1,30 @@
-import type { NextPage } from 'next'
-import StyledButton from '../components/StyledButton'
-import { Button } from '@mui/material'
+import { useState, useEffect, useRef } from 'react';
+import type { NextPage } from 'next';
+import Script from 'next/script';
+import StyledButton from '../components/StyledButton';
+import { Button } from '@mui/material';
+import StyledEmotionButton from '../components/StyledEmotionButton';
 
-
-const SsrPage: NextPage<{ title: string }> = ({ title }) => {
+const SsrPage: NextPage<{ name: string }> = ({ name }) => {
+  console.log('yoo');
   return (
-    <div>
-      <p>{title}</p>
-      <StyledButton onStyledButtonClick={() => console.log('HEY!')}>Sup</StyledButton>
-      <Button variant="contained">Contained</Button>
-    </div>
-  )
-}
+    <>
+      <Script async src={`https://cse.google.com/cse.js?cx=${process.env.NEXT_PUBLIC_CSE_CX}`} />
+      <div>
+        <p>{`YOOOOOOOO! ${name}：Ｄ`}</p>
+        <StyledButton onStyledButtonClick={() => console.log('HEY!')}>Sup</StyledButton>
+        <Button variant="contained">Contained</Button>
+        <StyledEmotionButton backgroundColor="salmon">Emotion</StyledEmotionButton>
+      </div>
+      <div className="gcse-search"></div>
+    </>
+  );
+};
 
 export async function getStaticProps() {
-  console.log('hohohoho')
-  return { props: { title: 'YOOOOOOOO!' } };
+  const res = await fetch('http://localhost:3000/api/hello');
+  const data = await res.json();
+  return { props: { name: data.name } };
 }
 
-export default SsrPage
+export default SsrPage;
