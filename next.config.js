15e4d8194f41c9d02d086dd49e4a581758da853e
kyo144 <kwsmcy@gmail.com>
2022-08-26 22:18:37 +0800

    chore: set react strict mode dynamically

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: process.env.NODE_ENV === 'production' ? true : false,
   swcMinify: true,
   compiler: {
     styledComponents: true,
